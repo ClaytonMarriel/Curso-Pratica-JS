ef7aa74fa0660ff5cb82aa0a7efd472bce8b13e0
Clayton Marriel <50889610+ClaytonMarriel@users.noreply.github.com>
2020-06-15 14:59:59 -0300

    git

---
+++ Exercicios_Módulos/5.js
@@ -0,0 +1,3 @@
+function valor(a) {
+    console.log
+}
---
+++ Objeto OO/oo.js
@@ -0,0 +1,27 @@
+/*
+
+Linguagem estruturada
+-Procedural - Função que manipula os dados
+processamento(valor1,valor2,valor3)
+
+
+OO -> Objeto que possui atributos e que também possuem comportamentos -- dados que tem funções
+objeto = { ob
+    valor1,
+    valor2,
+    valor3,
+    processamento(){...}
+}
+objeto.processamento 
+
+
+Principios importantes:
+1- Abstração: Converter "a vida real" no desenvolvimento de software
+2- Encapsulamento: Detalhes de implementação
+3- Herança (prototype):  Recebe atributos e comportamentos de um objeto PAI
+3.1: Herança: é um / Composição: tem um:
+4- Polimorfismo: Multiplas formas
+
+
+
+*/
